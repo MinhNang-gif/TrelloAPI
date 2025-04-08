927984a1ddf9c76da9522d419ce99a31f2cedbbc
MinhNang-gif <leminhnang2k7@gmail.com>
2025-04-08 13:23:46 +0700

    express routing

--- src/server.js
+++ src/server.js
@@ -1,16 +1,15 @@
 /* eslint-disable no-console */
 import express from 'express'
 import exitHook from 'async-exit-hook'
-import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
+import { APIs_V1 } from '~/routes/v1/index'
 
 
 const START_SERVER = () => {
   const app = express()
 
-  app.get('/', (req, res) => {
-    res.end('<h1>Hello World!</h1><hr>')
-  })
+  app.use('/v1', APIs_V1)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`3. Hi ${process.env.AUTHOR}, Back-end server is running at http://${ env.APP_HOST }:${ env.APP_PORT }/`)
