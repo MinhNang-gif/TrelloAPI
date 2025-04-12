764556412ce24573de56595f394d1cfc5759ed47
MinhNang-gif <leminhnang2k7@gmail.com>
2025-04-12 23:25:04 +0700

    error handling

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,6 +1,8 @@
 // Dung joi de validate du lieu
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
+
 
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
@@ -26,10 +28,9 @@ const createNew = async (req, res, next) => {
     // Validate du lieu hop le thi cho request di tiep den tang Controller
     next()
   } catch (error) {
-    // Du lieu nay de tra ve postman
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message
-    })
+    const messageError = new Error(error).message
+    const customError = new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, messageError)
+    next(customError)
   }
 }
 
